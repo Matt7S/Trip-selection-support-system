70a08880bbc53720bf89750eeac1c2fce30360fc
Dawid Wiktorowski <101642832+Wektor169@users.noreply.github.com>
2024-01-16 21:26:41 +0100

    Update topsis

--- topsis.py
+++ topsis.py
@@ -1,40 +1,29 @@
 from typing import List
 import math
-import numpy as np
 
-dane = [[700,  2, 6, 8],
-        [250,  2, 4, 8],
-        [1200, 6, 9, 16],
-        [880,  4, 9, 12],
-        [450,  3, 8, 12],
-        [1900, 8, 10, 18]]
+def topsis(data: List[List[int]], lower_limits: List, upper_limits: List, weight_vector: List, benefit_attributes: List)-> List:
 
 
-lower_limits_ = [0, 0, 0, 0]
-upper_limits_ = [1800, 7, 9, 16]
-weight_vector_ = [1/4, 1/4, 1/4, 1/4]
-criterion_ = [0, 1, 1, 1]
-
-
-def topsis(data: List[List[int]], lower_limits: List, upper_limits: List, weight_vector: List, criterion: List):
     # determining the number of alternatives and criteria
     number_of_alternatives = len(data)
-    number_of_criteria = len(data[0])
+    number_of_criteria = len(data[0]) -1
 
     # Checking the correctness of sizes
-    if all(len(actual_list) == number_of_criteria for actual_list in [lower_limits, upper_limits, weight_vector, criterion]):
+    if all(len(actual_list)  == number_of_criteria for actual_list in [lower_limits, upper_limits, weight_vector, benefit_attributes]):
 
         # Establishing a list of compatible alternatives
         compatible_alternatives = [1 for i in range(number_of_alternatives)]
         compatible_data = []
+        compatible_id = []
         for i in range(number_of_alternatives):
-            for j in range(number_of_criteria):
-                if all([data[i][j] >= lower_limits[j], data[i][j] <= upper_limits[j], compatible_alternatives[i] > 0]):
+            for j in range(0, number_of_criteria):
+                if all([data[i][j+1] >= lower_limits[j], data[i][j+1] <= upper_limits[j], compatible_alternatives[i] > 0]):
                     compatible_alternatives[i] = 1
                 else:
                     compatible_alternatives[i] = 0
             if compatible_alternatives[i] == 1:
-                compatible_data.append(data[i])
+                compatible_data.append(data[i][1:])
+                compatible_id.append(data[i][0])
 
         # Creating a normalized decision matrix taking into account the weight vector
         new_number_of_alternatives = len(compatible_data)
@@ -47,7 +36,7 @@ def topsis(data: List[List[int]], lower_limits: List, upper_limits: List, weight
             factor[j] = math.sqrt(factor[j])
         for j in range(new_number_of_criteria):
             for i in range(new_number_of_alternatives):
-                if criterion[j] == 1:
+                if benefit_attributes[j] == 1:
                     standardized_decision_matrix[i][j] = (1 - compatible_data[i][j] / factor[j]) * weight_vector[j]
                 else:
                     standardized_decision_matrix[i][j] = (compatible_data[i][j] / factor[j]) * weight_vector[j]
@@ -71,15 +60,12 @@ def topsis(data: List[List[int]], lower_limits: List, upper_limits: List, weight
 
         # Determination of the scoring coefficient
         scoring_factor = []
-        for i in range(new_number_of_criteria):
+        for i in range(new_number_of_alternatives):
             scoring_factor.append(distance_from_anti_ideal[i] / (distance_from_ideal[i] + distance_from_anti_ideal[i]))
-        print("przeszło do końca")
+        merged = list(zip(scoring_factor, compatible_id))
+        _, result = zip(*sorted(merged, key=lambda x: x[0]))
+        result = list(result)
+        return result
     else:
         print("Incompatible input data length")
-
-
-
-if __name__ == "__main__":
-    topsis(dane, lower_limits_, upper_limits_, weight_vector_, criterion_)
-
 
