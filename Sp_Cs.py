30c08b59c6a4d42c1b072d5f276be5e18db8b712
SzymonTokarzProgramista <149623510+SzymonTokarzProgramista@users.noreply.github.com>
2024-01-20 13:32:00 +0100

    ranking w 2d

--- Sp_Cs.py
+++ Sp_Cs.py
@@ -4,9 +4,9 @@ from scipy.spatial import Voronoi, distance
 def point_line_distance(point, line_start, line_end):
     return np.abs(np.cross(line_end - line_start, line_start - point)) / np.linalg.norm(line_end - line_start)
 
-def voronoi_distance(points, voronoi):
+def voronoi_distance(points):
     total_params_and_distances_sum = []
-
+    voronoi=Voronoi(points)
     for point in points:
         min_distance = float('inf')
         nearest_edge_params_sum = 0
@@ -23,17 +23,16 @@ def voronoi_distance(points, voronoi):
 
         total_params_and_distances_sum.append(nearest_edge_params_sum + min_distance)
 
-    return total_params_and_distances_sum
+        indices = list(range(len(total_params_and_distances_sum)))
+        sorted_indices = sorted(indices, key=lambda i: total_params_and_distances_sum[i])
+
+    return sorted_indices
 
 # Przykładowe punkty w przestrzeni 2D.
 sample_points = np.array([(2, 3), (5, 8), (8, 4), (9, 7), (12, 5)])
 
-# Tworzenie diagramu Voronoi.
-vor = Voronoi(sample_points)
-
 # Obliczanie sumy parametru krzywej Voronoi i odległości od niej dla każdego punktu.
-total_params_and_distances_sum = voronoi_distance(sample_points, vor)
+sorted= voronoi_distance(sample_points)
+print(sorted)
 
-# Wyświetlanie wyników dla każdego punktu.
-for i, point in enumerate(sample_points):
-    print(f"Punkt {i+1}: Suma parametru krzywej Voronoi i odległości od niej - {total_params_and_distances_sum[i]:.2f}")
+
