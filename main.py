8062d636050a0628790dcb49cc7c4a861d8cb280
Matt7S <98317236+Matt7S@users.noreply.github.com>
2024-01-23 13:47:08 +0100

    main add comments

--- main.py
+++ main.py
@@ -325,30 +325,31 @@ def open_UTA_star_window(r, minimum, benefit_attributes_):
 
 
 def open_SPCS_window(r, minimum, benefit_attributes_):
-
-    # przykładowe minimalne wartości dla każdej kategorii
+    # Sample minimum values for each category
     min_ranges = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]  
-    # przykładowe maksymalne wartości dla każdej kategorii
-    max_ranges = [1000, 1000, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100]  
+    # Sample maximum values for each category
+    max_ranges = [650, 2400, 11000, 95, 95, 91, 100, 95, 97, 99, 95, 100, 98] 
 
     def update_comboboxes(*args):
+        # This function updates the available options in comboboxes based on user selections.
         selected = set(cb.get() for cb in comboboxes)
         for cb in comboboxes:
             current = cb.get()
             cb['values'] = [c for c in criteria_labels if c not in selected or c == current]
 
-        # Aktualizowanie etykiet zakresów
+        # Update range labels for the selected category
         for i, cb in enumerate(comboboxes):
             selected_category_index = criteria_labels.index(cb.get()) if cb.get() in criteria_labels else -1
             if selected_category_index >= 0:
-                min_labels[i].config(text="Min: " + str(min_ranges[selected_category_index]))
-                max_labels[i].config(text="Max: " + str(max_ranges[selected_category_index]))
+                min_labels[i].config(text="Min: " + str(minimum[selected_category_index]))
+                max_labels[i].config(text="Max: " + str(maximum[selected_category_index]))
             else:
                 min_labels[i].config(text="Min:")
                 max_labels[i].config(text="Max:")
 
     
     def fun_method():
+        # This function is called when the "Generuj Ranking" button is clicked.
         ranking_area.delete('1.0', tk.END)
         criteria_idxs_ = []
         lower_limits_ = []
@@ -364,84 +365,88 @@ def open_SPCS_window(r, minimum, benefit_attributes_):
                 lower_limits_.append(lower_limit)
                 upper_limits_.append(upper_limit)
 
-
         if try_conv(lower_limits_) and try_conv(upper_limits_):
-            # Zakładam, że weight_vector_ i benefit_attributes_ są odpowiednio zdefiniowane
+            # Assuming that weight_vector_ and benefit_attributes_ are properly defined
             result_ = Sp_Cs.sp_cs(r[3], lower_limits_, upper_limits_, criteria_idxs_, benefit_attributes_)
-            if result_ == None:
+            if result_ is None:
                 ranking_area.insert(tk.END, "Niestety twoje kryteria są zbyt wąskie")
             else:
                 text = ""
                 for i in range(len(result_)):
                     text += f"{i + 1}. {r[1][result_[i]][1]}, {r[1][result_[i]][2]}\n"
                 ranking_area.insert(tk.END, text)
-
         else:
             messagebox.showwarning("Warning", "Wrong value entered!")
 
     
     def end_fullscreen(event=None):
+        # This function is called when the Escape key is pressed.
+        # It exits full-screen mode for the tkinter window.
         root.attributes('-fullscreen', False)
         return "break"
 
-
+    # Create a new tkinter window
     new_window = tk.Toplevel(root)
     new_window.title("SP_CS")
     new_window.bind("<Escape>", end_fullscreen)
-    #setting tkinter window size
+    # Set window size and fullscreen state
     new_window.geometry("%dx%d" % (global_window_width, global_window_height))
     new_window.state("zoomed")
 
+    # Create a label frame for criteria selection
     criteria_frame = tk.LabelFrame(new_window, text="Kryteria brane pod uwagę", padx=5, pady=5)
     criteria_frame.grid(row=0, column=0, sticky="news", padx=10, pady=5)
 
-    criteria_labels = r[2][1:]  # Zmienne kryteriów
+    # Get criteria labels from input data
+    criteria_labels = r[2][1:]
     comboboxes = []
     min_entries = []
     max_entries = []
 
-    # Dodaj etykiety zakresów w interfejsie użytkownika
+    # Create labels for minimum and maximum range
     min_labels = []
     max_labels = []
 
     for i in range(3):
-        # Dodanie etykiety "Wybierz kategorię"
+        # Create a label for selecting a category
         category_label = tk.Label(criteria_frame, text="Wybierz kategorię:")
         category_label.grid(row=3*i, column=1, padx=5, pady=2)
 
-        # Tworzenie i umieszczanie etykiet zakresów
+        # Create labels for minimum and maximum range values
         min_label = tk.Label(criteria_frame, text="Min:")
-        min_label.grid(row=3*i+1, column=2)  # umieszczenie etykiety "Min:" nad polem wpisywania
+        min_label.grid(row=3*i+1, column=2)
         min_labels.append(min_label)
 
         max_label = tk.Label(criteria_frame, text="Max:")
-        max_label.grid(row=3*i+1, column=3)  # umieszczenie etykiety "Max:" nad polem wpisywania
+        max_label.grid(row=3*i+1, column=3)
         max_labels.append(max_label)
 
-        # Tworzenie i umieszczanie comboboxów oraz pól wpisywania
+        # Create comboboxes and entry fields for user input
         cb = ttk.Combobox(criteria_frame, values=criteria_labels)
-        cb.grid(row=3*i+2, column=1, padx=5, pady=2)  # umieszczenie comboboxa pod etykietą "Wybierz kategorię"
+        cb.grid(row=3*i+2, column=1, padx=5, pady=2)
         cb.bind('<<ComboboxSelected>>', update_comboboxes)
         comboboxes.append(cb)
 
         min_entry = tk.Entry(criteria_frame, width=10)
-        min_entry.grid(row=3*i+2, column=2, padx=5, pady=2)  # umieszczenie pola wpisywania pod etykietą "Min:"
+        min_entry.grid(row=3*i+2, column=2, padx=5, pady=2)
         min_entries.append(min_entry)
 
         max_entry = tk.Entry(criteria_frame, width=10)
-        max_entry.grid(row=3*i+2, column=3, padx=5, pady=2)  # umieszczenie pola wpisywania pod etykietą "Max:"
+        max_entry.grid(row=3*i+2, column=3, padx=5, pady=2)
         max_entries.append(max_entry)
 
-
+    # Create a label frame for displaying the ranking
     ranking_frame = tk.LabelFrame(new_window, text="Ranking", padx=5, pady=5)
     ranking_frame.grid(row=0, column=1, rowspan=2, sticky="nsew", padx=10, pady=5)
     ranking_area = tk.Text(ranking_frame, wrap=tk.WORD)
     ranking_area.grid(row=0, column=0, sticky="nsew")
 
+    # Create a scrollbar for the ranking area
     scrollbar_ranking = tk.Scrollbar(ranking_frame, command=ranking_area.yview)
     ranking_area['yscrollcommand'] = scrollbar_ranking.set
     scrollbar_ranking.grid(row=0, column=1, sticky='nsew')
 
+    # Create buttons for generating ranking and returning to the main window
     button_frame = tk.Frame(new_window)
     button_frame.grid(row=1, column=0, padx=5, pady=5)
 
@@ -451,87 +456,94 @@ def open_SPCS_window(r, minimum, benefit_attributes_):
     return_button = tk.Button(button_frame, text="Powrót do okna głównego", command=new_window.destroy)
     return_button.grid(row=0, column=1, pady=5)
 
+    # Configure grid column and row weights for resizing
     new_window.grid_columnconfigure(1, weight=1)
     new_window.grid_rowconfigure(0, weight=1)
     ranking_frame.grid_columnconfigure(0, weight=1)
     ranking_frame.grid_rowconfigure(0, weight=1)
 
-    update_comboboxes()  # Aktualizuj comboboxy przy inicjalizacji
+    update_comboboxes()  # Update comboboxes initially
     
 
 if __name__ == "__main__":
+    # Function to exit fullscreen mode
     def end_fullscreen(event=None):
         root.attributes('-fullscreen', False)
         return "break"
-    
-    def open_website(event):
-        webbrowser.open("https://github.com/Matt7S/Trip-selection-support-system")  # Zastąp adresem URL docelowego
 
+    # Function to change cursor appearance to 'hand2' (clicking pointer)
     def change_cursor(event):
-        event.widget.config(cursor="hand2")  # Zmiana kształtu kursora na "hand2" (wskaźnik do klikania)
+        event.widget.config(cursor="hand2")
 
-
+    # Extract data from a database (assuming this function exists)
     r = extract_data.get_data_from_database()
 
+    # List representing benefit attributes
     benefit_attributes_ = [0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
 
+    # Transpose a list of data and find minimum and maximum values
     transposed_list = list(zip(*r[3]))[1:]
     minimum = [min(column) for column in transposed_list]
     maximum = [max(column) for column in transposed_list]
+    print(maximum)
 
+    # Create the main Tkinter window
     root = tk.Tk()
-    #setting tkinter window size
+    # Set the size of the tkinter window
     root.geometry("%dx%d" % (global_window_width, global_window_height))
     root.state("zoomed")
-    root.title("Systemy Wspomagania Decyzji")
+    root.title("Decision Support Systems")
 
-    # Załaduj obraz tła
-    width_background_image = 900
-    height_background_image = 500
+    # Load a background image
+    my_size = 250
+    width_background_image = 5*my_size
+    height_background_image = 3*my_size
     background_image = Image.open("start_page_photo.png")
     background_image = background_image.resize((width_background_image, height_background_image))
     background_photo = ImageTk.PhotoImage(background_image)
     background_label = tk.Label(root, image=background_photo)
     background_label.place(x=0, y=0, relwidth=1, relheight=1)
 
-    #background_label.bind("<Button-1>", open_website)  # Obsługa kliknięcia lewym przyciskiem myszy
+    # Create a label for the main title
+    tk.Label(root, text="Gdzie świat poniesie Cię dziś?", font=("Helvetica", 40)).pack()
 
-    # Dodaj etykietę "Wybierz metodę"
-    tk.Label(root, text="Gdzie świat poniesie cię dziś?", font=("Helvetica", 40)).pack()
+    # Define methods as a dictionary with method names as keys and associated functions as values
+    methods = {
+        "Topsis": lambda: open_topsis_window(r, minimum, benefit_attributes_),
+        "RSM": lambda: open_RSM_window(r, minimum, benefit_attributes_),
+        "UTA Star": lambda: open_UTA_star_window(r, minimum, benefit_attributes_),
+        "SP_CS": lambda: open_SPCS_window(r, minimum, benefit_attributes_)
+    }
 
-    methods = {"Topsis": lambda: open_topsis_window(r, minimum, benefit_attributes_),
-               "RSM": lambda: open_RSM_window(r, minimum, benefit_attributes_),
-               "UTA Star": lambda: open_UTA_star_window(r, minimum, benefit_attributes_),
-               "SP_CS": lambda: open_SPCS_window(r, minimum, benefit_attributes_)}
-    
-
-    label_reminder = tk.Label(root, text="Powyżej wybierz odpowiednią metodę", font=("Helvetica", 12))
+    # Create a label as a reminder for method selection
+    label_reminder = tk.Label(root, text="Powyżej wybierz odpowiednią metodę.", font=("Helvetica", 12))
     label_reminder.pack(side=tk.BOTTOM, pady=10)
 
+    # Create a frame for buttons
     button_frame = tk.Frame(root, background='white')
     button_frame.pack(side=tk.BOTTOM, pady=20)
 
-    # Lista kolorów tęczy
+    # List of rainbow colors
     rainbow_colors = ["gray", "gray", "blue", "gray", "gray", "gray"]
     color_cycle = cycle(rainbow_colors)
 
-
+    # Function to handle mouse entering a button
     def on_enter(event):
         next_color = next(color_cycle)
-        event.widget.config(bg=next_color, fg='white', width=21, height=2)  # Zmień kolor tła i rozmiar po najechaniu myszką
+        event.widget.config(bg=next_color, fg='white', width=21, height=2)
         change_cursor(event)
-        
 
+    # Function to handle mouse leaving a button
     def on_leave(event):
-        event.widget.config(bg='black', fg='white', width=20, height=2)  # Przywróć tło i rozmiar do oryginalnych po opuszczeniu myszki
+        event.widget.config(bg='black', fg='white', width=20, height=2)
         event.widget.config(cursor="")
 
-
+    # Create buttons for each method and bind mouse events
     for method, action in methods.items():
         button = tk.Button(button_frame, text=method, command=action, height=2, width=20, bg='black', fg='white', font=("Helvetica",12,'bold'))
         button.pack(side=tk.LEFT, padx=30, pady=0)
-        button.bind("<Enter>", on_enter)  # Dodaj obsługę najechania myszką
-        button.bind("<Leave>", on_leave)  # Dodaj obsługę opuszczenia myszki
+        button.bind("<Enter>", on_enter)
+        button.bind("<Leave>", on_leave)
 
-
+    # Start the Tkinter main loop
     root.mainloop()
